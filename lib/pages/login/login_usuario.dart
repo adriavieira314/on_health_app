17f8fea8e068062f11b0aa41719729adafe74331
adriavieira314 <adriavieira731@gmail.com>
2023-04-23 18:24:27 -0400

    initial commit

---
+++ lib/pages/login/login_usuario.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LoginUsuario extends StatelessWidget {
+  const LoginUsuario({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
