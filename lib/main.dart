e5ab0bdfb14fb87a5ed0dc917196fc1bdb323371
adriavieira314 <adriavieira731@gmail.com>
2023-04-23 20:08:45 -0400

    layout login usuario

--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'On Health',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green.shade900),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.green.shade900,
+          secondary: Colors.white,
+        ),
         useMaterial3: false,
       ),
       home: const LoginUsuario(),
--- lib/pages/login/login_usuario.dart
+++ lib/pages/login/login_usuario.dart
@@ -1,10 +1,127 @@
 import 'package:flutter/material.dart';
+import 'package:on_health_app/components/text_form_field.dart';
 
 class LoginUsuario extends StatelessWidget {
   const LoginUsuario({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                children: [
+                  Image.asset(
+                    'assets/images/logo_1.png',
+                    width: 300,
+                    height: 300,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 18.0),
+                    child: Form(
+                      child: Column(
+                        children: [
+                          const TextFormFieldComponent(
+                            label: 'Número do CPF',
+                            hintText: 'Digite o seu CPF',
+                          ),
+                          const TextFormFieldComponent(
+                            label: 'Senha de acesso',
+                            hintText: 'Digite a sua senha',
+                          ),
+                          Row(
+                            children: [
+                              ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                  minimumSize: Size(
+                                    (MediaQuery.of(context).size.width * 0.5) -
+                                        23,
+                                    45,
+                                  ),
+                                  backgroundColor:
+                                      Theme.of(context).colorScheme.secondary,
+                                  elevation: 8,
+                                  shadowColor: Colors.grey,
+                                ),
+                                onPressed: () => {},
+                                child: Text(
+                                  'Cadastrar',
+                                  style: TextStyle(
+                                    color:
+                                        Theme.of(context).colorScheme.primary,
+                                    fontSize: 18.0,
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(width: 10.0),
+                              ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                  minimumSize: Size(
+                                    (MediaQuery.of(context).size.width * 0.5) -
+                                        23,
+                                    45,
+                                  ),
+                                  elevation: 8,
+                                  shadowColor: Colors.grey,
+                                ),
+                                onPressed: () => {},
+                                child: const Text(
+                                  'Entrar',
+                                  style: TextStyle(
+                                    fontSize: 18.0,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: Size(
+                        (MediaQuery.of(context).size.width * 0.5) - 23,
+                        45,
+                      ),
+                      backgroundColor: Theme.of(context).colorScheme.secondary,
+                      elevation: 8,
+                      shadowColor: Colors.grey,
+                    ),
+                    onPressed: () => {},
+                    child: Text(
+                      'Sou administrador',
+                      style: TextStyle(
+                        color: Theme.of(context).colorScheme.primary,
+                        fontSize: 18.0,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 20.0),
+                    child: Text(
+                      'Esqueci a senha',
+                      style: TextStyle(
+                        color: Theme.of(context).colorScheme.primary,
+                        fontSize: 18.0,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
