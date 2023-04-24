acbdd6d7887510af1d60c8cd2be75665f5bbc6da
adriavieira314 <adriavieira731@gmail.com>
2023-04-23 23:08:27 -0400

    submetendo formulario

--- lib/pages/login/login_usuario.dart
+++ lib/pages/login/login_usuario.dart
@@ -1,15 +1,36 @@
 import 'package:flutter/material.dart';
-import 'package:on_health_app/components/text_form_field.dart';
 
-class LoginUsuario extends StatelessWidget {
+class LoginUsuario extends StatefulWidget {
   const LoginUsuario({super.key});
 
   @override
+  State<LoginUsuario> createState() => _LoginUsuarioState();
+}
+
+class _LoginUsuarioState extends State<LoginUsuario> {
+  final _passwordFocus = FocusNode();
+  final _formData = <String, Object>{};
+  final _formKey = GlobalKey<FormState>();
+
+  void _submitForm() {
+    final isValid = _formKey.currentState?.validate() ?? false;
+
+    if (!isValid) {
+      return;
+    }
+
+    _formKey.currentState?.save();
+    print(_formData);
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final mediaQuery = MediaQuery.of(context);
+
     return Scaffold(
       body: SingleChildScrollView(
         child: SizedBox(
-          height: MediaQuery.of(context).size.height,
+          height: mediaQuery.size.height,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
@@ -18,65 +39,146 @@ class LoginUsuario extends StatelessWidget {
                   Image.asset(
                     'assets/images/logo_1.png',
                     width: 300,
-                    height: 300,
+                    height: 280,
                   ),
                   Padding(
                     padding: const EdgeInsets.symmetric(horizontal: 18.0),
                     child: Form(
+                      key: _formKey,
                       child: Column(
                         children: [
-                          const TextFormFieldComponent(
-                            label: 'Número do CPF',
-                            hintText: 'Digite o seu CPF',
+                          Padding(
+                            padding: const EdgeInsets.symmetric(
+                              vertical: 10.0,
+                            ),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.only(
+                                      left: 15.0, bottom: 8.0),
+                                  child: Text(
+                                    'Número do CPF',
+                                    style: TextStyle(
+                                      color:
+                                          Theme.of(context).colorScheme.primary,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 18,
+                                    ),
+                                  ),
+                                ),
+                                TextFormField(
+                                  decoration: const InputDecoration(
+                                    hintText: 'Digite o seu CPF',
+                                    border: OutlineInputBorder(),
+                                  ),
+                                  textInputAction: TextInputAction.next,
+                                  onSaved: (cpf) =>
+                                      _formData['cpf'] = cpf ?? '',
+                                  onFieldSubmitted: (_) {
+                                    FocusScope.of(context)
+                                        .requestFocus(_passwordFocus);
+                                  },
+                                  validator: (value) {
+                                    final cpf = value ?? '';
+
+                                    if (cpf.trim().isEmpty) {
+                                      return 'Campo obrigatório.';
+                                    }
+
+                                    return null;
+                                  },
+                                ),
+                              ],
+                            ),
                           ),
-                          const TextFormFieldComponent(
-                            label: 'Senha de acesso',
-                            hintText: 'Digite a sua senha',
+                          Padding(
+                            padding: const EdgeInsets.symmetric(
+                              vertical: 10.0,
+                            ),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.only(
+                                      left: 15.0, bottom: 8.0),
+                                  child: Text(
+                                    'Senha de acesso',
+                                    style: TextStyle(
+                                      color:
+                                          Theme.of(context).colorScheme.primary,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 18,
+                                    ),
+                                  ),
+                                ),
+                                TextFormField(
+                                  decoration: const InputDecoration(
+                                    hintText: 'Digite a sua senha',
+                                    border: OutlineInputBorder(),
+                                  ),
+                                  textInputAction: TextInputAction.done,
+                                  focusNode: _passwordFocus,
+                                  onSaved: (password) =>
+                                      _formData['password'] = password ?? '',
+                                  validator: (value) {
+                                    final password = value ?? '';
+
+                                    if (password.trim().isEmpty) {
+                                      return 'Campo obrigatório.';
+                                    }
+
+                                    return null;
+                                  },
+                                ),
+                              ],
+                            ),
                           ),
-                          Row(
-                            children: [
-                              ElevatedButton(
-                                style: ElevatedButton.styleFrom(
-                                  minimumSize: Size(
-                                    (MediaQuery.of(context).size.width * 0.5) -
-                                        23,
-                                    45,
+                          Padding(
+                            padding: const EdgeInsets.only(top: 10.0),
+                            child: Row(
+                              children: [
+                                ElevatedButton(
+                                  style: ElevatedButton.styleFrom(
+                                    minimumSize: Size(
+                                      (mediaQuery.size.width * 0.5) - 23,
+                                      45,
+                                    ),
+                                    backgroundColor:
+                                        Theme.of(context).colorScheme.secondary,
+                                    elevation: 8,
+                                    shadowColor: Colors.grey,
                                   ),
-                                  backgroundColor:
-                                      Theme.of(context).colorScheme.secondary,
-                                  elevation: 8,
-                                  shadowColor: Colors.grey,
-                                ),
-                                onPressed: () => {},
-                                child: Text(
-                                  'Cadastrar',
-                                  style: TextStyle(
-                                    color:
-                                        Theme.of(context).colorScheme.primary,
-                                    fontSize: 18.0,
-                                  ),
-                                ),
-                              ),
-                              const SizedBox(width: 10.0),
-                              ElevatedButton(
-                                style: ElevatedButton.styleFrom(
-                                  minimumSize: Size(
-                                    (MediaQuery.of(context).size.width * 0.5) -
-                                        23,
-                                    45,
-                                  ),
-                                  elevation: 8,
-                                  shadowColor: Colors.grey,
-                                ),
-                                onPressed: () => {},
-                                child: const Text(
-                                  'Entrar',
-                                  style: TextStyle(
-                                    fontSize: 18.0,
+                                  onPressed: () => {},
+                                  child: Text(
+                                    'Cadastrar',
+                                    style: TextStyle(
+                                      color:
+                                          Theme.of(context).colorScheme.primary,
+                                      fontSize: 18.0,
+                                    ),
                                   ),
                                 ),
-                              ),
-                            ],
+                                const SizedBox(width: 10.0),
+                                ElevatedButton(
+                                  style: ElevatedButton.styleFrom(
+                                    minimumSize: Size(
+                                      (mediaQuery.size.width * 0.5) - 23,
+                                      45,
+                                    ),
+                                    elevation: 8,
+                                    shadowColor: Colors.grey,
+                                  ),
+                                  onPressed: _submitForm,
+                                  child: const Text(
+                                    'Entrar',
+                                    style: TextStyle(
+                                      fontSize: 18.0,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
                           )
                         ],
                       ),
@@ -89,10 +191,7 @@ class LoginUsuario extends StatelessWidget {
                 children: [
                   ElevatedButton(
                     style: ElevatedButton.styleFrom(
-                      minimumSize: Size(
-                        (MediaQuery.of(context).size.width * 0.5) - 23,
-                        45,
-                      ),
+                      minimumSize: const Size(200, 50),
                       backgroundColor: Theme.of(context).colorScheme.secondary,
                       elevation: 8,
                       shadowColor: Colors.grey,
