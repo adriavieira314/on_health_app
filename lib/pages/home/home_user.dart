9d2961e9f50edf524f80674e4190a8a710ba4f7b
adriavieira314 <adriavieira731@gmail.com>
2023-04-25 02:04:03 -0400

    segunda versao layout menu usuario

--- lib/pages/home/home_user.dart
+++ lib/pages/home/home_user.dart
@@ -19,75 +19,198 @@ class HomeUser extends StatelessWidget {
     };
 
     return Scaffold(
-      body: SafeArea(
-        child: Column(
+      appBar: AppBar(
+        toolbarHeight: 80,
+        title: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 20.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Container(
-                    margin: const EdgeInsets.fromLTRB(18.0, 0.0, 18.0, 25.0),
-                    padding: const EdgeInsets.symmetric(vertical: 15.0),
-                    width: mediaQuery.size.width,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(30),
-                      color: Colors.blue.shade100,
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        CircleAvatar(
-                          backgroundColor: Colors.brown.shade800,
-                          radius: 30,
-                          child: const Text('AH'),
-                        ),
-                        Text(
-                          user_info['nome']!,
-                          style: const TextStyle(
-                            fontSize: 25.0,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        Text(
-                          'CPF: ${user_info['cpf']!}',
-                          style: const TextStyle(
-                            fontSize: 20.0,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  TextInfo(
-                    label: 'Nascimento: ',
-                    info: user_info['birthdate']!,
-                  ),
-                  const Divider(),
-                  TextInfo(
-                    label: 'IMC: ',
-                    info: user_info['imc']!,
-                  ),
-                  const Divider(),
-                  TextInfo(
-                    label: 'Mãe: ',
-                    info: user_info['parent']!,
-                  ),
-                  const Divider(),
-                  TextInfo(
-                    label: 'Endereço: ',
-                    info: user_info['street']!,
-                  ),
-                  const Divider(),
-                  TextInfo(
-                    label: 'Bairro: ',
-                    info: user_info['address']!,
-                  ),
-                ],
+            Text(
+              user_info['nome']!,
+              style: const TextStyle(
+                fontSize: 22.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              'CPF: ${user_info['cpf']!}',
+              style: const TextStyle(
+                fontSize: 20.0,
               ),
             ),
           ],
         ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              backgroundColor: Colors.blue.shade50,
+              radius: 25,
+              child: const Text('AH'),
+            ),
+          )
+        ],
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(
+            bottom: Radius.circular(20),
+          ),
+        ),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TextInfo(
+                  label: 'Nascimento: ',
+                  info: user_info['birthdate']!,
+                ),
+                const Divider(),
+                TextInfo(
+                  label: 'IMC: ',
+                  info: user_info['imc']!,
+                ),
+                const Divider(),
+                TextInfo(
+                  label: 'Mãe: ',
+                  info: user_info['parent']!,
+                ),
+                const Divider(),
+                TextInfo(
+                  label: 'Endereço: ',
+                  info: user_info['street']!,
+                ),
+                const Divider(),
+                TextInfo(
+                  label: 'Bairro: ',
+                  info: user_info['address']!,
+                ),
+              ],
+            ),
+          ),
+          DefaultTabController(
+            length: 2,
+            initialIndex: 0,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Container(
+                  color: Colors.blue.shade50,
+                  child: const TabBar(
+                    labelColor: Colors.green,
+                    unselectedLabelColor: Colors.black,
+                    tabs: [
+                      Tab(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Padding(
+                              padding: EdgeInsets.only(right: 8.0),
+                              child: Icon(Icons.bloodtype),
+                            ),
+                            Text(
+                              'Diabetes',
+                              style: TextStyle(fontSize: 22.0),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Tab(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Padding(
+                              padding: EdgeInsets.only(right: 8.0),
+                              child: Icon(Icons.bloodtype),
+                            ),
+                            Text(
+                              'Hipertensão',
+                              style: TextStyle(fontSize: 22.0),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  height: 400, //height of TabBarView
+                  decoration: const BoxDecoration(
+                    // color: Colors.red,
+                    border: Border(
+                      top: BorderSide(color: Colors.grey, width: 0.5),
+                    ),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 20.0),
+                    child: TabBarView(
+                      children: [
+                        Column(
+                          children: [
+                            TextInfo(
+                              label: 'Unidade de Saúde: ',
+                              info: user_info['address']!,
+                            ),
+                            const Divider(),
+                            TextInfo(
+                              label: 'Médico: ',
+                              info: user_info['address']!,
+                            ),
+                            const Divider(),
+                            TextInfo(
+                              label: 'Último atendimento: ',
+                              info: user_info['address']!,
+                            ),
+                            const Divider(),
+                            TextInfo(
+                              label: 'Valor da glicemia: ',
+                              info: user_info['address']!,
+                            ),
+                            const Divider(),
+                            TextInfo(
+                              label: 'Medicação recebida: ',
+                              info: user_info['address']!,
+                            ),
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            TextInfo(
+                              label: 'Unidade de Saúde: ',
+                              info: user_info['address']!,
+                            ),
+                            const Divider(),
+                            TextInfo(
+                              label: 'Médico: ',
+                              info: user_info['address']!,
+                            ),
+                            const Divider(),
+                            TextInfo(
+                              label: 'Último atendimento: ',
+                              info: user_info['address']!,
+                            ),
+                            const Divider(),
+                            TextInfo(
+                              label: 'Valor da glicemia: ',
+                              info: user_info['address']!,
+                            ),
+                            const Divider(),
+                            TextInfo(
+                              label: 'Medicação recebida: ',
+                              info: user_info['address']!,
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
