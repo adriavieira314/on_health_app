c6fafb194133820b3c0301648752254d7b1b3ae4
adriavieira314 <adriavieira731@gmail.com>
2023-04-27 17:01:56 -0400

    lista de agendamentos

--- lib/pages/home/user/agendamentos_user.dart
+++ lib/pages/home/user/agendamentos_user.dart
@@ -5,9 +5,68 @@ class AgendamentosUser extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Text(
-      'Index 1: Agendamentos',
-      style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+    return ListView.builder(
+      padding: const EdgeInsets.all(8),
+      itemCount: 3,
+      itemBuilder: (BuildContext context, int index) {
+        return Card(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ListTile(
+                leading: const Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.date_range),
+                  ],
+                ),
+                title: const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 10.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Clínico Geral',
+                        style: TextStyle(
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      Text(
+                        'Adriano Silva',
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                subtitle: const Text(
+                  '27/04/2023',
+                  style: TextStyle(
+                    fontSize: 15.0,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                trailing: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () => {},
+                      style: ElevatedButton.styleFrom(
+                        shape: const StadiumBorder(),
+                        minimumSize: const Size(120, 50),
+                      ),
+                      child: const Text('Confirmar'),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
