7899c33a1e46c4e260c2f17836000bf2d3e5ee18
adriavieira314 <adriavieira731@gmail.com>
2023-06-12 20:28:29 -0400

    adicionado localidade

--- lib/components/patient_info.dart
+++ lib/components/patient_info.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:on_health_app/components/home/text_info.dart';
 import 'package:on_health_app/models/agendamentos.dart';
+import 'package:on_health_app/providers/localization_provider.dart';
 import 'package:on_health_app/utils/capitalize.dart';
+import 'package:provider/provider.dart';
 
 class PatientInfo extends StatelessWidget {
   const PatientInfo({super.key});
@@ -69,6 +71,56 @@ class PatientInfo extends StatelessWidget {
               label: 'Profissional: ',
               info: '${agendamento.nmProfSaude!.capitalizeByWord()}',
             ),
+            ChangeNotifierProvider<LocalizationProvider>(
+              create: (context) => LocalizationProvider(),
+              child: Builder(builder: (context) {
+                final local = context.watch<LocalizationProvider>();
+
+                return local.erro != ''
+                    ? Text(local.erro)
+                    : local.lat != 0.0
+                        ? Column(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(top: 25.0),
+                                child: Text(
+                                  'Localidade:',
+                                  style: const TextStyle(
+                                    fontSize: 18.0,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                              const Divider(),
+                              TextInfo(
+                                label: 'Latitude: ',
+                                info: '${local.lat.toString()}',
+                              ),
+                              const Divider(),
+                              TextInfo(
+                                label: 'Longitude: ',
+                                info: '${local.long.toString()}',
+                              ),
+                              const Divider(),
+                              TextInfo(
+                                label: 'Rua: ',
+                                info: '${local.street}',
+                              ),
+                              const Divider(),
+                              TextInfo(
+                                label: 'Bairro: ',
+                                info: '${local.sublocality}',
+                              ),
+                              const Divider(),
+                              TextInfo(
+                                label: 'CEP: ',
+                                info: '${local.zipCode}',
+                              ),
+                            ],
+                          )
+                        : Container();
+              }),
+            )
           ],
         ),
       ),
