3ce7b45ad3e75f5c9c6bd80ebb9c9f388099d606
adriavieira314 <adriavieira731@gmail.com>
2023-09-17 21:04:31 -0400

    ajustes layout SelectMunicipioPage

--- lib/pages/select_municipio.dart
+++ lib/pages/select_municipio.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:on_health_app/models/municipios.dart';
 import 'package:on_health_app/providers/municipios_provider.dart';
+import 'package:on_health_app/utils/app_routes.dart';
 import 'package:on_health_app/utils/constants.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -54,66 +55,106 @@ class Municipios_DropdownButtonState extends State<MunicipiosDropdownButton> {
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 18.0),
         child: Center(
-          child: Column(
-            children: [
-              Text(
-                'Selecione o município em que você está:',
-                style: const TextStyle(
-                  fontSize: 22.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              loading
-                  ? Center(
-                      child: CircularProgressIndicator(),
-                    )
-                  : listaMunicipios.municipios!.length == 0
-                      ? Center(
-                          child: CircularProgressIndicator(),
-                        )
-                      : Container(
-                          width: mediaQuery.size.width * 0.9,
-                          margin: EdgeInsets.only(top: 30.0),
-                          child: DecoratedBox(
-                            decoration: BoxDecoration(
-                              color: Colors.white,
-                              border:
-                                  Border.all(color: Colors.black38, width: 2),
-                              borderRadius: BorderRadius.circular(50),
-                            ),
-                            child: Padding(
-                              padding: const EdgeInsets.fromLTRB(
-                                  10.0, 8.0, 10.0, 8.0),
-                              child: DropdownButton<Municipios>(
-                                value: dropdownValue,
-                                elevation: 16,
-                                style: const TextStyle(
-                                    color: Colors.green, fontSize: 20.0),
-                                underline: Container(
-                                  height: 0,
-                                  color: Colors.deepPurpleAccent,
-                                ),
-                                onChanged: (Municipios? value) {
-                                  setState(() async {
-                                    dropdownValue = value!;
-                                    print(dropdownValue);
-                                    await fetchIdIBGE(dropdownValue);
-                                  });
-                                },
-                                items: listaMunicipios.municipios!
-                                    .map<DropdownMenuItem<Municipios>>(
-                                        (Municipios value) {
-                                  return DropdownMenuItem<Municipios>(
-                                    value: value,
-                                    child: Text(value.municipio!),
-                                  );
-                                }).toList(),
+          child: loading
+              ? Center(
+                  child: CircularProgressIndicator(),
+                )
+              : Column(
+                  children: [
+                    Image.asset(
+                      'assets/images/logo_1.png',
+                      width: 300,
+                      height: 220,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 50.0),
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(bottom: 20.0),
+                            child: Text(
+                              'Selecione o município em que você está:',
+                              style: TextStyle(
+                                color: Theme.of(context).colorScheme.primary,
+                                fontSize: 20.0,
+                                fontWeight: FontWeight.bold,
                               ),
                             ),
                           ),
-                        ),
-            ],
-          ),
+                          listaMunicipios.municipios!.length == 0
+                              ? Center(
+                                  child: CircularProgressIndicator(),
+                                )
+                              : Container(
+                                  width: mediaQuery.size.width,
+                                  child: DecoratedBox(
+                                    decoration: BoxDecoration(
+                                      color: Colors.white,
+                                      border: Border.all(
+                                          color: Colors.black38, width: 1),
+                                      borderRadius: BorderRadius.circular(10),
+                                    ),
+                                    child: Padding(
+                                      padding: const EdgeInsets.fromLTRB(
+                                        10.0,
+                                        8.0,
+                                        10.0,
+                                        8.0,
+                                      ),
+                                      child: DropdownButton<Municipios>(
+                                        value: dropdownValue,
+                                        elevation: 16,
+                                        style: const TextStyle(
+                                            color: Colors.green,
+                                            fontSize: 20.0),
+                                        underline: Container(
+                                          height: 0,
+                                          color: Colors.deepPurpleAccent,
+                                        ),
+                                        onChanged: (Municipios? value) {
+                                          setState(() async {
+                                            dropdownValue = value!;
+                                            print(dropdownValue);
+                                            await fetchIdIBGE(dropdownValue);
+                                          });
+                                        },
+                                        items: listaMunicipios.municipios!
+                                            .map<DropdownMenuItem<Municipios>>(
+                                                (Municipios value) {
+                                          return DropdownMenuItem<Municipios>(
+                                            value: value,
+                                            child: Text(value.municipio!),
+                                          );
+                                        }).toList(),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 20.0),
+                            child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                minimumSize: Size(mediaQuery.size.width, 55),
+                                elevation: 8,
+                                shadowColor: Colors.grey,
+                              ),
+                              onPressed: () => {
+                                Navigator.of(context)
+                                    .pushNamed(AppRoutes.AUTH_OR_HOME)
+                              },
+                              child: const Text(
+                                'Próximo',
+                                style: TextStyle(
+                                  fontSize: 18.0,
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
         ),
       ),
     );
